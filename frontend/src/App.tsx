c18e54127dd5ffe2ac35667f59310deb1cb8bd99
tooooo1 <wer4272@nate.com>
2022-08-25 03:28:47 +0900

    remove: navbar 제거

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
-import Navbar from '@/components/Navbar';
-import { Login, MyPage, NotFound } from '@/pages';
+import { LocationLevel, Login, MyPage, NotFound } from '@/pages';
 
 const App = () => {
   return (
@@ -9,10 +8,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/locationlevel" element={<LocationLevel />} />
         <Route path="/404" element={<NotFound />} />
         <Route path="/*" element={<Navigate replace to="/404" />} />
       </Routes>
-      <Navbar />
     </BrowserRouter>
   );
 };
