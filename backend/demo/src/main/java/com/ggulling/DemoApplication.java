30c17138dc47a7714d50b5a7fde9632786294958
soleu <dlthf555@gmail.com>
2022-08-25 22:48:41 +0900

    refactor : 불필요한 코드 제거

--- backend/demo/src/main/java/com/ggulling/DemoApplication.java
+++ backend/demo/src/main/java/com/ggulling/DemoApplication.java
@@ -3,7 +3,9 @@ package com.ggulling;
 import com.zkdlu.apiresponsespringbootstarter.autoconfig.EnableResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @EnableResponse
 @SpringBootApplication
 public class DemoApplication {
