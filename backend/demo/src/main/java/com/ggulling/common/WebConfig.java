1e82e912d419263f72fcbf116e96494e8417db5c
soleu <dlthf555@gmail.com>
2022-08-26 01:48:12 +0900

    chore

--- backend/demo/src/main/java/com/ggulling/common/WebConfig.java
+++ backend/demo/src/main/java/com/ggulling/common/WebConfig.java
@@ -3,17 +3,12 @@ package com.ggulling.common;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-    private List<String> whiteList = new ArrayList<>();
-
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
