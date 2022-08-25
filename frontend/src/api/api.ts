d749caafcc8ec9881c248562239c9b35bf23b702
tooooo1 <wer4272@nate.com>
2022-08-25 17:33:29 +0900

    fix: 임시 주석

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,19 +1,19 @@
 import axios from 'axios';
 axios.defaults.baseURL = import.meta.env.VITE_REACT_APP_URL;
 
-const instance = axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_URL,
-});
+// const instance = axios.create({
+//   baseURL: import.meta.env.VITE_REACT_APP_URL,
+// });
 
-const signUp = (nickname: string) => {
-  return instance({
-    url: `/api/v1/auth/signup`,
-  });
-};
+// const signUp = (nickname: string) => {
+//   return instance({
+//     url: `/api/v1/auth/signup`,
+//   });
+// };
 
-const login = () => {
-  return instance({
-    url: '/api/v1/auth/signin',
-    // data: nickname,
-  });
-};
+// const login = () => {
+//   return instance({
+//     url: '/api/v1/auth/signin',
+//     // data: nickname,
+//   });
+// };
