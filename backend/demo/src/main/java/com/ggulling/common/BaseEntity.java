49969666276f550fff03056ae88d64f6f64ad4cb
soleu <dlthf555@gmail.com>
2022-08-25 11:44:21 +0900

    fix : farm - base entitiy

---
+++ backend/demo/src/main/java/com/ggulling/common/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.ggulling.common;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @CreatedDate
+    protected LocalDateTime createdAt;
+    @LastModifiedDate
+    protected LocalDateTime updateAt;
+}
