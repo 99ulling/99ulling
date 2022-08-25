5db4418ea735edaa27d3c091be7d886a2338e26d
soleu <dlthf555@gmail.com>
2022-08-26 01:46:27 +0900

    cors

--- backend/demo/src/main/java/com/ggulling/common/WebConfig.java
+++ backend/demo/src/main/java/com/ggulling/common/WebConfig.java
@@ -15,16 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
     private List<String> whiteList = new ArrayList<>();
 
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        whiteList.addAll(List.of("/css/**", "/*.ico", "/error", "/refresh", "/docs/**", "/signIn",
-                "/api/v1/auth/all",
-                "/api/v1/farm/**",
-                "/api/v1/history/**",
-                "/api/v1/sharing/**",
-                "/api/v1/user/**"));
-    }
-
-    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "https://g9ulling.run.goorm.io")
