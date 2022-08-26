95cba1ff98f959020d670618361648a78eec977d
tooooo1 <wer4272@nate.com>
2022-08-26 13:11:49 +0900

    feat: 실패 경우 추가

--- frontend/src/pages/FamerUpload.tsx
+++ frontend/src/pages/FamerUpload.tsx
@@ -33,11 +33,16 @@ const FamerUpload = () => {
         longitude: 33.273398,
         nickname: famerName,
         userType: 'FAMER',
-      }).then(() => {
-        alert('성공적으로 등록했어요.');
-        navigate('/famerappcompleted');
-        setShareState(ggul);
-      });
+      })
+        .then(() => {
+          alert('성공적으로 등록했어요.');
+          setShareState(ggul);
+        })
+        .then(() => navigate('/famerappcompleted'))
+        .catch(() => {
+          alert('데이터 저장에 실패했어요.\n다시 시도해주세요.');
+          navigate('/');
+        });
     }
   };
 
@@ -46,26 +51,8 @@ const FamerUpload = () => {
       <Back />
       <img width="100%" src="/sharing99.png" alt="sharing99.png" />
       <Wrapper>
-        <FamerName>귤마마씨</FamerName>
-        <FamerAddress>제주시 서귀포시 농장로 342길 2</FamerAddress>
-        <Time>
-          이용 시간을 알려주세요.
-          <svg
-            style={{ marginLeft: '6px' }}
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M9.32131 4.32512L2.00031 11.6461V14.2551H4.60931L11.9293 6.93312L9.32131 4.32512ZM10.0563 3.59012L11.9013 1.74512L14.5093 4.35312L12.6653 6.19812L10.0563 3.59012Z"
-              fill="#727272"
-            />
-          </svg>
-        </Time>
+        <FamerName>{famerName}</FamerName>
+        <FamerAddress>{famerAddressState}</FamerAddress>
         <div style={{ width: '80%', padding: '2rem 0' }}>
           <InputSetting
             style={{ textAlign: 'center', padding: '1.6rem 0' }}
@@ -73,12 +60,7 @@ const FamerUpload = () => {
             onChange={onChangeGgul}
           />
         </div>
-
-        <Introduction>
-          제주의 뜨거운 태양과 맑은 물을 먹고
-          <br /> 자라나 맛 좋고 싱싱한 감귤 농장입니다.
-        </Introduction>
-        <div style={{ width: '100%', padding: '1.2rem 42px 0 42px' }}>
+        <ButtonPosition>
           <DefaultButton
             backgroundColor="#F57D14"
             onClick={() => handleSubmit()}
@@ -86,7 +68,7 @@ const FamerUpload = () => {
           >
             확인하기
           </DefaultButton>
-        </div>
+        </ButtonPosition>
       </Wrapper>
     </div>
   );
@@ -136,4 +118,11 @@ const InputSetting = styled(Input)`
   padding-left: 20px;
   border-color: none;
   text-align: 'center';
+`;
+
+const ButtonPosition = styled.div`
+  position: absolute;
+  bottom: 60px;
+  width: 100%;
+  padding: 1.2rem 42px 0 42px;
 `;
