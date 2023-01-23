8d720172691fb9c4f3c0d07d324c5e2c561a97e5
tooooo1 <wer4272@nate.com>
2023-01-24 05:09:48 +0900

    feat: kakao map api

---
+++ frontend/src/components/Map.tsx
@@ -0,0 +1,100 @@
+import styled from '@emotion/styled';
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    kakao: any;
+  }
+}
+
+interface MapProps {
+  latitude: number;
+  longitude: number;
+}
+
+const Map = ({ latitude, longitude }: MapProps) => {
+  useEffect(() => {
+    const mapScript = document.createElement('script');
+
+    mapScript.async = true;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${
+      import.meta.env.VITE_REACT_KAKAO_API
+    }&autoload=false`;
+
+    document.head.appendChild(mapScript);
+
+    const onLoadKakaoMap = () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById('map');
+        const options = {
+          center: new window.kakao.maps.LatLng(latitude, longitude),
+        };
+        const map = new window.kakao.maps.Map(container, options);
+
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition((position) => {
+            const { latitude, longitude } = position.coords;
+
+            const locPosition = new window.kakao.maps.LatLng(
+                latitude,
+                longitude
+              ),
+              message = '<div style="padding:5px;">여기에 계신가요?!</div>';
+
+            displayMarker(locPosition, message);
+          });
+        } else {
+          const locPosition = new window.kakao.maps.LatLng(
+              33.450701,
+              126.570667
+            ),
+            message = 'geolocation을 사용할수 없어요..';
+
+          displayMarker(locPosition, message);
+        }
+
+        function displayMarker(position: any, message: string) {
+          const imageSrc = '/public/marker.png',
+            imageSize = new window.kakao.maps.Size(64, 69),
+            imageOption = { offset: new window.kakao.maps.Point(27, 69) };
+
+          const image = new window.kakao.maps.MarkerImage(
+            imageSrc,
+            imageSize,
+            imageOption
+          );
+          const marker = new window.kakao.maps.Marker({
+            map,
+            position,
+            image,
+          });
+
+          const content = message,
+            removable = true;
+
+          const infowindow = new window.kakao.maps.InfoWindow({
+            content,
+            removable,
+          });
+
+          infowindow.open(map, marker);
+
+          map.setCenter(position);
+        }
+      });
+    };
+    mapScript.addEventListener('load', onLoadKakaoMap);
+
+    return () => mapScript.removeEventListener('load', onLoadKakaoMap);
+  }, [latitude, longitude]);
+
+  return <MapContainer id="map" />;
+};
+
+const MapContainer = styled.div`
+  width: 325px;
+  height: 325px;
+`;
+
+export default Map;
--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,5 +1,6 @@
 interface ImportMeta {
   env: {
     VITE_REACT_APP_URL?: string;
+    VITE_REACT_KAKAO_API?: string;
   };
 }
