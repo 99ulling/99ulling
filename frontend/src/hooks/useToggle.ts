7e5d9bdd9b0c96ede7124279a983a245b9c60e1a
tooooo1 <wer4272@nate.com>
2023-01-28 21:59:13 +0900

    refactor: custom hook (toggle)

---
+++ frontend/src/hooks/useToggle.ts
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+const useToggle = (
+  initialValue = true
+): [boolean, (state: boolean) => void] => {
+  const [toggle, setToggle] = useState(initialValue);
+
+  const toggleValue = (state: boolean) => setToggle(state ? state : !toggle);
+
+  return [toggle, toggleValue];
+};
+
+export default useToggle;
