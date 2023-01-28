fc150c47acc59dbf7a9424dfebacdd3768dcfe87
tooooo1 <wer4272@nate.com>
2023-01-28 22:25:24 +0900

    refactor: main as remove

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 
 import App from './App';
-import { globalStyles } from './globalStyles';
+import { globalStyles } from './globalStyles.js';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     {globalStyles}
     <RecoilRoot>
